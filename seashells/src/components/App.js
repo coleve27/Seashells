b6dc7030ee205b19510dcec6e2602f29c41cb2c0
Evelyn Coleman <coleve27@gmail.com>
2018-04-04 19:57:30 -0500

    going with bootstrap instead

--- seashells/src/components/App.js
+++ seashells/src/components/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Button from "./RaisedButton";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 class App extends Component {
   render() {
-    return (
-      <MuiThemeProvider>
-        {" "}
-        <Button>hello </Button>
-      </MuiThemeProvider>
-    );
+    return <div> Hello </div>;
   }
 }
 
